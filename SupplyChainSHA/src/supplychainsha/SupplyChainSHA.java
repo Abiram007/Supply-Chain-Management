eccc35b8c93e99cc8ea454a267f71334a993c9f8
Abiram <38308166+Abiram007@users.noreply.github.com>
2024-02-22 14:43:54 +0530

    Add files via upload

---
+++ SupplyChainSHA/src/supplychainsha/SupplyChainSHA.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package supplychainsha;
+import retailer.Retailer;
+import supplier.Supplier;
+import manufacturer.Manufacturer;
+import distributor.Distributor;
+import dlt1.DLT1;
+import dlt2.DLT2;
+import logistics.Logistics;
+
+/**
+ *
+ * @author ramus
+ */
+public class SupplyChainSHA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SupplyChainSHAFrame scframe = new SupplyChainSHAFrame();
+        scframe.setTitle("SupplyChain Management");
+        scframe.setVisible(true);
+        scframe.setResizable(false);
+        
+        
+        
+//        Retailer.main(args);
+//        Supplier.main(args);
+//        Manufacturer.main(args);
+//        Distributor.main(args);
+//        DLT1.main(args);
+//        DLT2.main(args);
+//        Logistics.main(args);
+    }
+    
+}
